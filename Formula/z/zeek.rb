bfb2f6c3841f9f9ff8f32cdbe8ea49375f237c4b
Benjamin Bannier <benjamin.bannier@corelight.com>
2025-05-20 10:24:38 +0200

    fixup! fixup! zeek 7.2.0

--- Formula/z/zeek.rb
+++ Formula/z/zeek.rb
@@ -48,10 +48,19 @@ class Zeek < Formula
     inreplace "auxil/spicy/hilti/toolchain/src/config.cc.in", "${CMAKE_CXX_COMPILER}", ENV.cxx
 
     # Benchmarks are not installed, but building them on Linux breaks in the
-    # bundled google-benchmark dependency.
+    # bundled google-benchmark dependency. Exclude the benchmark targets an
+    # their library dependency.
+    inreplace "auxil/spicy/hilti/runtime/CMakeLists.txt",
+      "add_executable(hilti-rt-fiber-benchmark src/benchmarks/fiber.cc)",
+      "add_executable(hilti-rt-fiber-benchmark EXCLUDE_FROM_ALL src/benchmarks/fiber.cc)"
     inreplace "auxil/spicy/spicy/runtime/tests/benchmarks/CMakeLists.txt",
       "add_executable(spicy-rt-parsing-benchmark parsing.cc ${_generated_sources})",
       "add_executable(spicy-rt-parsing-benchmark EXCLUDE_FROM_ALL parsing.cc ${_generated_sources})"
+    inreplace "auxil/spicy/3rdparty/justrx/CMakeLists.txt", "add_subdirectory(src/tests)", ""
+    ("auxil/spicy/3rdparty/CMakeLists.txt").write <<~CMAKE
+      set_target_properties(benchmark PROPERTIES EXCLUDE_FROM_ALL ON)
+      set_target_properties(benchmark_main PROPERTIES EXCLUDE_FROM_ALL ON)
+    CMAKE
 
     system "cmake", "-S", ".", "-B", "build",
                     "-DBROKER_DISABLE_TESTS=on",
